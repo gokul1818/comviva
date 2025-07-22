eb95490f251914693c96a69bb48bffc887bc88dd
Sowmiya <info.sowmiyag@gmail.com>
2025-07-22 20:32:07 +0530

    iOS issues fixed

--- ios/comviva/AppDelegate.swift
+++ ios/comviva/AppDelegate.swift
@@ -1,32 +1,70 @@
+//import UIKit
+//import React
+//import Firebase
+//
+//@main
+//class AppDelegate: RCTAppDelegate {
+//  override func application(
+//    _ application: UIApplication,
+//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+//  ) -> Bool {
+//    
+//    // Configure Firebase first
+//    FirebaseApp.configure()
+//    
+//    self.moduleName = "comviva"
+//    self.dependencyProvider = RCTAppDependencyProvider()
+//    self.initialProps = [:]
+//
+//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//  }
+//
+//  override func sourceURL(for bridge: RCTBridge) -> URL? {
+//    return self.bundleURL()
+//  }
+//
+//  override func bundleURL() -> URL? {
+//#if DEBUG
+//    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+//#else
+//    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+//#endif
+//  }
+//}
 import UIKit
 import React
-import React_RCTAppDelegate
-import ReactAppDependencyProvider
-import FirebaseCore
+import Firebase
 
-@main
-class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+
     FirebaseApp.configure()
-    self.moduleName = "comviva"
-    self.dependencyProvider = RCTAppDependencyProvider()
 
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
-    self.initialProps = [:]
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let bridge = RCTBridge(delegate: self, launchOptions: launchOptions)
+    let rootView = RCTRootView(bridge: bridge!, moduleName: "comviva", initialProperties: nil)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+
+    return true
   }
+}
 
-  
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
-
-  override func bundleURL() -> URL? {
+extension AppDelegate: RCTBridgeDelegate {
+  func sourceURL(for bridge: RCTBridge!) -> URL! {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
   }
 }
